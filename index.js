f85bac7f899e4e573f8b6d1b936ec2180fbc255f
infinitemax <98976626+infinitemax@users.noreply.github.com>
2023-10-25 17:35:43 +0100

    Revert "Backend add authentication of users"

--- index.js
+++ index.js
@@ -7,39 +7,15 @@ const helmet = require('helmet');
 const cors = require('cors');
 const createError = require('http-errors');
 require("dotenv").config();
-const eventRoutes = require("./routes/events");
-const { User } = require("./models/tempUsers")
-
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    })
-    .then(console.log(`connected to the database at mongodb`))
-    .catch(error => console.log(error))
 
 
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
 
-app.use(morgan("dev"));
-app.use(express.json({ extended: false }));
+app.use(morgan());
+app.use(express.json());
 app.use(helmet());
-
-
-app.post("/auth", async (req, res, next) => {
-    console.log("logging in")
-    const loginName = req.body.username
-    try {
-        const user = await User
-
-    } catch (error) {
-        return next(createError(500, "Server error"))
-    }
-})
-
-
-app.use("/", eventRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
