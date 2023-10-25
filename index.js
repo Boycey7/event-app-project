28e80b76d894a51fab5c2badc7a2ee0d756b4778
infinitemax <m.brookman.byrne@gmail.com>
2023-10-25 13:26:57 +0100

    add connection to mongodb database

--- index.js
+++ index.js
@@ -7,14 +7,22 @@ const helmet = require('helmet');
 const cors = require('cors');
 const createError = require('http-errors');
 require("dotenv").config();
+const eventRoutes = require("./routes/events");
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    })
+    .then(console.log(`connected to the database at mongodb`))
+    .catch(error => console.log(error))
 
 
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
 
-app.use(morgan());
-app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.json({ extended: false }));
 app.use(helmet());
 
 app.listen(port, () => {
