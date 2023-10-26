a2082add499cdd26e0f48f363102edacac2a3be6
infinitemax <m.brookman.byrne@gmail.com>
2023-10-26 16:22:58 +0100

    finalise endpoint to serve all posts by a specific user

--- controllers/events.js
+++ controllers/events.js
@@ -3,104 +3,138 @@ const mongoose = require("mongoose");
 const { Event } = require("../models/events");
 const helmet = require("helmet");
 const { User } = require("../models/tempUsers");
-const { v4: uuidv4 } =require("uuid");
-
-
-
+const { v4: uuidv4 } = require("uuid");
 
 // get all events
 exports.getEvents = async (req, res, next) => {
-    try {
-        const events = await Event.find({});
-        res.status(200).json(events);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "a server error has occurred" });
-        return next(createError(500, "a server error has occured"));
-    }
+  try {
+    await Event.find({})
+      .populate("creator", "email")
+      .then((events) => {
+        res.status(200).json({
+          count: events.length,
+          events: events.map((event) => {
+            return {
+              event,
+              eventCreator: event.creator._id.toString(),
+            };
+          }),
+        });
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "a server error has occurred" });
+    return next(createError(500, "a server error has occured"));
+  }
 };
 
 // add an event
 exports.addEvent = async (req, res, next) => {
-    const data = req.body;
+  const data = req.body;
 
-    try {
-        const newEvent = new Event(data);
-        await newEvent.save();
-        res.status(200).json({
-            message: "Event created successfully!",
-            event: newEvent,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "a server error has occurred" });
-        return next(createError(500, "a server error has occured"));
-    }
+  try {
+    const newEvent = new Event(data);
+    await newEvent.save();
+    res.status(200).json({
+      message: "Event created successfully!",
+      event: newEvent,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "a server error has occurred" });
+    return next(createError(500, "a server error has occured"));
+  }
 };
 
 // update an event
 exports.updateEventById = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const changes = req.body;
-        await Event.findByIdAndUpdate(id, changes);
-        const updatedEvent = await Event.findById(id);
-        res.status(200).json({
-            message: "Event updated successfully",
-            newEvent: updatedEvent
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "a server error has occurred" });
-        return next(createError(500, "a server error has occured"));
-    }
+  try {
+    const { id } = req.params;
+    const changes = req.body;
+    await Event.findByIdAndUpdate(id, changes);
+    const updatedEvent = await Event.findById(id);
+    res.status(200).json({
+      message: "Event updated successfully",
+      newEvent: updatedEvent,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "a server error has occurred" });
+    return next(createError(500, "a server error has occured"));
+  }
 };
 
 // delete an event
 
 exports.deleteEventById = async (req, res, next) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        if (!id) {
-            return next(createError(400, "Bad request"));
-        }
-
-        const deletedEvent = await Event.findByIdAndDelete(id);
-
-        res.send({
-            message: "event deleted",
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "a server error has occurred" });
-        return next(createError(500, "a server error has occured"));
+    if (!id) {
+      return next(createError(400, "Bad request"));
     }
+
+    const deletedEvent = await Event.findByIdAndDelete(id);
+
+    res.send({
+      message: "event deleted",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "a server error has occurred" });
+    return next(createError(500, "a server error has occured"));
+  }
 };
 
-// get all users 
+// get all users
 exports.getAllUsers = async (req, res, next) => {
-    try {
-        // const allUsers = await User.find();
-        res.send("hello")
-    } catch (error) {
-        console.log(error);
-        res.send("a server error has occurred");
-        return next(createError(500, "a server error has occured"));
-    }
-}
+  try {
+    const allUsers = await User.find();
+    res.send(allUsers);
+  } catch (error) {
+    console.log(error);
+    res.send("a server error has occurred");
+    return next(createError(500, "a server error has occured"));
+  }
+};
 
-// get events by user - NOT WORKING!
-// exports.getEventsByUser = async (req, res, next) => {
-//     const id = req.params.id;
+// get all events by a given user
+exports.getEventsByUser = async (req, res, next) => {
+  const { id } = req.params;
 
-//     try {
-//         // this isn't working! Might need to try the populate method!
-//         const eventsByUser = Event.find({ creator: id });
-//         res.send(eventsByUser);
-//     } catch (error) {
-//         console.log(error);
-//         res.send("a server error has occurred");
-//         return next(createError(500, "a server error has occured"));
-//     }
-// };
+  try {
+    // make an array of all events, inc their creator
+    // then filter based on teh creator
+    // does it already create an array?
+    
+    const allEvents = await Event.find({}).populate("creator")
+
+    const usersEvents = allEvents.filter(event => {
+        return event.creator._id.toString() === id
+    })
+
+
+    res.status(200).send(usersEvents);
+
+
+    
+    // const allEvents = [];
+    // await Event.find({})
+    //   .populate("creator")
+    //   .then(
+    //     (events) => {
+    //         res.status(200).json({
+    //             events: events.filter((event) => {
+    //                 event.creator._id === id;
+    //             })
+    //         }
+    //     })
+    
+        
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "a server error has occurred" });
+    return next(createError(500, "a server error has occured"));
+  }
+};
--- routes/events.js
+++ routes/events.js
@@ -5,10 +5,10 @@ const { addEvent, getEvents, getEventsByUser, deleteEventById, updateEventById, 
 
 router.get("/", getEvents) // get all events
 router.post("/add", addEvent) // add an event
-// router.get("/user/:id", getEventsByUser) // get events by user - currently by id, but would be good to do it by username - doesn't work atm...
+router.get("/user/:id", getEventsByUser) // get events by user - currently by id, but would be good to do it by username - doesn't work atm...
 router.delete("/delete/:id", deleteEventById) // delete an event
 router.put("/update/:id", updateEventById) // updat an event
 
-// router.get("/users", getAllUsers) // get a list of all users
+router.get("/users", getAllUsers) // get a list of all users
 
 module.exports = router
